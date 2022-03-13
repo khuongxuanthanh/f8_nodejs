002b5480679bc06e03efcfaeda5ed79ca310bdb2
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-13 18:52:25 +0700

    up heroku

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
+const path = require('path');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const morgan = require('morgan');
-const path = require('path');
-const app = express();
 const port = process.env.PORT || 6789;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
+
 app.use(morgan('combined'));
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'resources/views'));
 app.set('layout', 'layouts/main');
+app.set('view engine', 'ejs');
 
-app.get('/',function (req, res) { 
+app.get('/', (req, res) => { 
   res.render('home');
 });
-app.get('/News',function (req, res) { 
+app.get('/News', (req, res) => { 
   res.render('news');
 });
 
-app.listen(port,function (err) {
-  console.log('Example app listening on port http://locahost:${port}')
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port htpp://locahost: ${port}`)
+})
