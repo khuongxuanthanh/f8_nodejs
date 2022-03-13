820ecef6d578b39d3a86137ec24fb4ded35b3feb
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-13 17:05:42 +0700

    boostrap4

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 6789;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'resources/views'));
+app.set('layout', 'layouts/main');
+
+app.get('/', (req, res) => { 
+  res.render('home');
+});
+app.get('/News', (req, res) => { 
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://locahost:${port}`)
+});
