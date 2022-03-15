751c9b64b3f936b562548ef371f87d58f369f414
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-15 20:46:12 +0700

    Refactor code: create routes, app/controllers foders

--- src/index.js
+++ src/index.js
@@ -5,21 +5,24 @@ const morgan = require('morgan');
 const port = process.env.PORT || 6789;
 
 const app = express();
+const route = require('./routes');
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'resources/views'));
 app.set('layout', 'layouts/main');
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => { 
-  res.render('home');
-});
-app.get('/News', (req, res) => { 
-  res.render('news');
-});
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port htpp://locahost: ${port}`)
-})
+  console.log(`Example app listening on port htpp://locahost:${port}`)
+});
