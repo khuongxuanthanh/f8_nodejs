a724bd02d1a0a1864e35365f0d24f85d2d9634c9
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-19 23:54:45 +0700

    Install prettie, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
+const path = require('path');
 const express = require('express');
+const { engine } = require('express-handlebars');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-import { engine } from 'express-handlebars';
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+// Template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+        app.set('view engine', 'hbs');
+        app.set('views', path.join(__dirname, 'resources/views'));
 
-app.set('views', './views');
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-  })
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
